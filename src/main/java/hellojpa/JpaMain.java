12eb84e978cb6f927d8f3e65c3674891b73a2af0
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-25 11:44:54 +0900

    2022-07-25 자바 ORM 표준 JPA 프로그래밍 - 기본편 (Hello JPA - 프로젝트 생성 / 애플리케이션 개발)

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,43 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //EntityManager를 꺼내고 나서 실제 동작하는 코드를 작성함.
+        //*** code ***//
+
+        try {
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
