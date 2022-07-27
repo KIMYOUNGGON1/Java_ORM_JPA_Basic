f12cf849896440ec4773e6fbfac5f290b8465c06
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-27 13:45:27 +0900

    2022-07-27 자바 ORM 표준 JPA 프로그래밍 - 기본편 (준영속 상태)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,14 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            //영속
 
-            em.flush();
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            em.clear();
 
             System.out.println("==============================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
