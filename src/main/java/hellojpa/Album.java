35e461470ef1b7d6ced8fc58241e522c14a7b1f3
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-29 16:22:47 +0900

    2022-07-29 자바 ORM 표준 JPA 프로그래밍 - 기본편 (상속관계 매핑)

---
+++ src/main/java/hellojpa/Album.java
@@ -0,0 +1,12 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Album extends Item {
+
+    private String artist;
+}
