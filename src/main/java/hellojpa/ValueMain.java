4d060ab6fd8676594d0c11ef02999efd7f84ff6c
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-08-12 11:54:23 +0900

    2022-08-12 자바 ORM 표준 JPA 프로그래밍 - 기본편 (값 타입의 비교)

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,19 +4,16 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
-        Integer a = new Integer(10);
-        Integer b = a;
+        int a = 10;
+        int b = 10;
 
-//        int a = 10;
-//        int b = a;
-//
-//        a = 20;
+        System.out.println("a == b : " + (a == b));
 
-//        a.setValue(20);
-//        와 같이 변경하면 둘 다 20 출력
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("address1 == address2 : " + (address1 == address2 ));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2))) ;
 
     }
 
